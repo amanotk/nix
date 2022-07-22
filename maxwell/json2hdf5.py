f96343e5e7986593dfb0d9e4fbe050138ccc5d45
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2022-07-22 12:33:26 +0900

    Added functionarity to create VDS

--- maxwell/json2hdf5.py
+++ maxwell/json2hdf5.py
@@ -11,7 +11,7 @@ import json
 import h5py
 
 _DEBUG = True
-
+FMT_CHUNKID = '%.8d'
 
 def json2hdf5(jsonfile, hdffile=None, verbose=True):
     if hdffile is None:
@@ -58,13 +58,33 @@ def json2hdf5(jsonfile, hdffile=None, verbose=True):
     #
     # create hdf5
     #
-    with h5py.File(hdffile, 'w') as h5fp, \
-         open(rawfile, 'r') as rawfp:
+    with h5py.File(hdffile, 'w') as h5fp:
+        pass
+
+    # create dataset
+    group = ''
+    add_dataset(obj, hdffile, rawfile, byteorder, order, group, verbose)
+
+    # create virtual dataset for chunked dataset
+    chunkmap = 'chunkmap'
+    chunked = 'chunked'
+    chunk = add_chunkmap(obj, hdffile, rawfile, byteorder, order, chunkmap, verbose)
+    if chunk:
+        dataset = create_chunked_dataset(hdffile, chunkmap, chunked, verbose)
+        create_vds(hdffile, chunkmap, chunked, dataset, verbose)
+
+    if verbose:
+        print('done !')
+
+
+def add_dataset(obj, hdffile, rawfile, byteorder, order, group='', verbose=True):
+    with h5py.File(hdffile, 'r+') as h5fp, open(rawfile, 'r') as rawfp:
         # attribute
         attribute = obj.get('attribute', [])
-        for name in attribute:
-            attr = attribute.get(name)
+        for attr_name in attribute:
+            attr = attribute.get(attr_name)
             data = read_data(rawfp, attr, byteorder)
+            name = '/'.join([group, attr_name])
             h5fp.attrs.create(name, data)
             if verbose:
                 print('  - attribute "{}" has been created '.format(name), end='')
@@ -72,20 +92,141 @@ def json2hdf5(jsonfile, hdffile=None, verbose=True):
 
         # dataset
         dataset = obj.get('dataset', [])
-        for name in dataset:
-            data = dataset.get(name)
+        for ds_name in dataset:
+            data = dataset.get(ds_name)
             offset, dsize, dtype, shape = read_info(data, byteorder)
             if order == 0:
-                # assume column-major (fortran-style) shape
                 shape = shape[::-1]
             ext = ((rawfile, offset, dsize), )
+            name = '/'.join([group, ds_name])
             h5fp.create_dataset(name, shape=shape, dtype=dtype, external=ext)
             if verbose:
                 print('  - dataset "{}" has been created '.format(name), end='')
                 print('with dtype = "{}" and shape = "{}"'.format(dtype, shape))
 
-    if verbose:
-        print('done !')
+
+def add_chunkmap(obj, hdffile, rawfile, byteorder, order, group='', verbose=True):
+    chunkmap = obj.get('chunkmap', None)
+    if chunkmap is None:
+        return False
+
+    with h5py.File(hdffile, 'r+') as h5fp, open(rawfile, 'r') as rawfp:
+        for ds_name in ('rank', 'coord', 'chunkid'):
+            if not ds_name in chunkmap:
+                continue
+            offset, dsize, dtype, shape = read_info(chunkmap[ds_name], byteorder)
+            if order == 0:
+                shape = shape[::-1]
+            ext = ((rawfile, offset, dsize), )
+            name = '/'.join([group, ds_name])
+            h5fp.create_dataset(name, shape=shape, dtype=dtype, external=ext)
+            if verbose:
+                print('  - dataset "{}" has been created '.format(name), end='')
+                print('with dtype = "{}" and shape = "{}"'.format(dtype, shape))
+
+    return True
+
+
+def is_chunked_dataset(dataset, cshape):
+    dshape = dataset.shape
+    status1 = (dshape[0] == np.prod(cshape))
+    status2 = np.alltrue([d%c == 0 for d, c in zip(dshape[1:4], cshape[0:3])])
+    return (status1 and status2)
+
+
+def is_external_contiguous(external):
+    status = True
+    filename, offset, size = external[0]
+    for ext in external[1:]:
+        status = status and (filename == ext[0])
+        status = status and (offset + size == ext[1])
+        offset = ext[1]
+        size = ext[2]
+    return status
+
+
+def create_chunked_dataset(hdffile, chunkmap, chunked, verbose=True):
+    chunked_dataset = []
+    with h5py.File(hdffile, 'r+') as h5fp:
+        cmap = h5fp.get(chunkmap, None)
+        csh = cmap.get('chunkid')[()].shape
+        for name, data in h5fp.items():
+            #
+            # check dataset
+            #
+            if name == chunkmap:
+                # ignore chunkmap
+                continue
+            if not is_chunked_dataset(data, csh):
+                # check if the dataset may be divided into chunks
+                continue
+            if not is_external_contiguous(data.external):
+                # check if the external storage is contiguous in disk
+                continue
+            #
+            # now create chunked dataset
+            #
+            dshape = data.shape
+            rawfile = data.external[0][0]
+            chunked_offset = data.external[0][1]
+            for i in range(dshape[0]):
+                chunked_name = '/'.join([chunked, ('%s' + FMT_CHUNKID) % (name, i)])
+                chunked_shape = dshape[1:]
+                chunked_dtype = data.dtype
+                chunked_dsize = np.prod(chunked_shape) * chunked_dtype.itemsize
+                chunked_ext = ((rawfile, chunked_offset, chunked_dsize), )
+                kwargs = { 'name' : chunked_name,
+                           'shape' : chunked_shape,
+                           'dtype' : chunked_dtype,
+                           'external' : chunked_ext,
+                }
+                h5fp.create_dataset(**kwargs)
+                # move offset
+                chunked_offset = chunked_offset + chunked_dsize
+                if verbose:
+                    print('  - chunked dataset "{}" has been created '.format(chunked_name), end='')
+                    print('with dtype = "{}" and shape = "{}"'.format(chunked_dtype, chunked_shape))
+        chunked_dataset.append(name)
+    return chunked_dataset
+
+
+def create_vds(hdffile, chunkmap, chunked, dataset, verbose=True):
+    group = 'vds'
+    with h5py.File(hdffile, 'r+') as h5fp:
+        # get chunk ID
+        chunkid = h5fp.get('/'.join([chunkmap, 'chunkid']))[()]
+        # try to create VDS for each dataset
+        for ds in dataset:
+            prefix = '/'.join([chunked, ds]) + FMT_CHUNKID
+            srcfile = [prefix % i for i in range(chunkid.size)]
+            srcdata = [h5fp.get(src) for src in srcfile]
+            #
+            # determine data shape assuming 3D chunk
+            #
+            csh = list(srcdata[0].shape)
+            gsh = csh.copy()
+            gsh[0] = csh[0]*chunkid.shape[0]
+            gsh[1] = csh[1]*chunkid.shape[1]
+            gsh[2] = csh[2]*chunkid.shape[2]
+            csh = tuple(csh)          # shape of each chunk
+            gsh = tuple(gsh)          # shape of global array
+            dtype = srcdata[0].dtype  # datatype
+            # create VirtualLayout object and assign chunks
+            layout = h5py.VirtualLayout(shape=gsh, dtype=dtype)
+            for cz in range(chunkid.shape[0]):
+                zslice = slice(cz*csh[0], (cz+1)*csh[0])
+                for cy in range(chunkid.shape[1]):
+                    yslice = slice(cy*csh[1], (cy+1)*csh[1])
+                    for cx in range(chunkid.shape[2]):
+                        xslice = slice(cx*csh[2], (cx+1)*csh[2])
+                        src = h5py.VirtualSource(srcdata[chunkid[cz,cy,cx]])
+                        layout[zslice,yslice,xslice,:] = src
+            # create VirtualDataset using the layout
+            vds_name = '/'.join([group, ds])
+            h5fp.create_virtual_dataset(vds_name, layout)
+            if verbose:
+                print('  - virtual dataset "{}" has been created '.format(vds_name), end='')
+                print('with dtype = "{}" and shape = "{}"'.format(dtype, gsh))
 
 
 def read_data(fp, obj, byteorder):
