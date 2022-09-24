afb2eae064be597b108bad2fd387f6b19410d1c4
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2022-09-24 19:04:13 +0900

    Code cleanup

--- jsonio.cpp
+++ jsonio.cpp
@@ -1,6 +1,7 @@
 // -*- C++ -*-
 
 #include "jsonio.hpp"
+#include "debug.hpp"
 
 namespace jsonio
 {
@@ -274,38 +275,201 @@ void write_contiguous_at(MPI_File *fh, size_t *disp, void *data, const size_t si
   readwrite_contiguous_at(fh, disp, data, size, elembyte, elembyte, req, -1);
 }
 
-template <>
-void read_subarray(MPI_File *fh, size_t *disp, void *data, const int32_t ndim,
-                   const int32_t gshape[], const int32_t lshape[], const int32_t offset[],
-                   const int32_t elembyte, const int order);
-template <>
-void read_subarray(MPI_File *fh, size_t *disp, void *data, const size_t ndim, const size_t gshape[],
-                   const size_t lshape[], const size_t offset[], const size_t elembyte,
-                   const int order);
-template <>
-void write_subarray(MPI_File *fh, size_t *disp, void *data, const int32_t ndim,
-                    const int32_t gshape[], const int32_t lshape[], const int32_t offset[],
-                    const int32_t elembyte, const int order);
-template <>
-void write_subarray(MPI_File *fh, size_t *disp, void *data, const size_t ndim,
-                    const size_t gshape[], const size_t lshape[], const size_t offset[],
-                    const size_t elembyte, const int order);
-template <>
-void get_attribute(json &obj, string name, size_t &disp, int32_t &data);
-template <>
-void get_attribute(json &obj, string name, size_t &disp, int64_t &data);
-template <>
-void get_attribute(json &obj, string name, size_t &disp, float32 &data);
-template <>
-void get_attribute(json &obj, string name, size_t &disp, float64 &data);
-template <>
-void get_attribute(json &obj, string name, size_t &disp, int32_t length, int32_t *data);
-template <>
-void get_attribute(json &obj, string name, size_t &disp, int32_t length, int64_t *data);
-template <>
-void get_attribute(json &obj, string name, size_t &disp, int32_t length, float32 *data);
-template <>
-void get_attribute(json &obj, string name, size_t &disp, int32_t length, float64 *data);
+template <typename T1, typename T2, typename T3, typename T4, typename T5>
+void read_subarray(MPI_File *fh, size_t *disp, void *data, const T1 ndim, const T2 gshape[],
+                   const T3 lshape[], const T4 offset[], const T5 elembyte, const int order)
+{
+  if (order != MPI_ORDER_C && order != MPI_ORDER_FORTRAN) {
+    ERRORPRINT("Error: No such order available\n");
+  }
+
+  // convert to int32_t for MPI call
+  int32_t nd = static_cast<int32_t>(ndim);
+  int32_t eb = static_cast<int32_t>(elembyte);
+  int32_t gs[nd], ls[nd], os[nd];
+
+  size_t size = 1;
+  for (int i = 0; i < nd; i++) {
+    gs[i] = static_cast<int32_t>(gshape[i]);
+    ls[i] = static_cast<int32_t>(lshape[i]);
+    os[i] = static_cast<int32_t>(offset[i]);
+    size *= gshape[i];
+  }
+
+  readwrite_subarray(fh, disp, data, nd, gs, ls, os, eb, +1, order);
+  *disp += size * elembyte;
+}
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5>
+void write_subarray(MPI_File *fh, size_t *disp, void *data, const T1 ndim, const T2 gshape[],
+                    const T3 lshape[], const T4 offset[], const T5 elembyte, const int order)
+{
+  if (order != MPI_ORDER_C && order != MPI_ORDER_FORTRAN) {
+    ERRORPRINT("Error: No such order available\n");
+  }
+
+  // convert to int32_t for MPI call
+  int32_t nd = static_cast<int32_t>(ndim);
+  int32_t eb = static_cast<int32_t>(elembyte);
+  int32_t gs[nd], ls[nd], os[nd];
+
+  size_t size = 1;
+  for (int i = 0; i < nd; i++) {
+    gs[i] = static_cast<int32_t>(gshape[i]);
+    ls[i] = static_cast<int32_t>(lshape[i]);
+    os[i] = static_cast<int32_t>(offset[i]);
+    size *= gshape[i];
+  }
+
+  readwrite_subarray(fh, disp, data, nd, gs, ls, os, eb, -1, order);
+  *disp += size * elembyte;
+}
+
+void put_metadata(json &obj, string name, string dtype, string desc, const size_t disp,
+                  const size_t size, const int32_t ndim, const int32_t dims[])
+{
+  obj[name]["datatype"]    = dtype;
+  obj[name]["description"] = desc;
+  obj[name]["offset"]      = disp;
+  obj[name]["size"]        = size;
+  obj[name]["ndim"]        = ndim;
+  obj[name]["shape"]       = json::array();
+
+  for (int i = 0; i < ndim; i++) {
+    obj[name]["shape"].push_back(dims[i]);
+  }
+}
+
+void put_metadata(json &obj, string name, string dtype, string desc, const size_t disp,
+                  const size_t size)
+{
+  const int ndim    = 1;
+  const int dims[1] = {1};
+  put_metadata(obj, name, dtype, desc, disp, size, ndim, dims);
+}
+
+void get_metadata(json &obj, string name, string &dtype, string &desc, size_t &disp, size_t &size,
+                  int32_t &ndim, int32_t dims[])
+{
+  dtype = obj[name]["datatype"].get<string>();
+  desc  = obj[name]["description"].get<string>();
+  disp  = obj[name]["offset"].get<size_t>();
+  size  = obj[name]["size"].get<size_t>();
+  ndim  = obj[name]["ndim"].get<int>();
+
+  auto v = obj[name]["shape"];
+  for (int i = 0; i < ndim; i++) {
+    dims[i] = v[i].get<int>();
+  }
+}
+
+void get_metadata(json &obj, string name, string &dtype, string &desc, size_t &disp, size_t &size)
+{
+  int32_t ndim;
+  int32_t dims[1];
+  get_metadata(obj, name, dtype, desc, disp, size, ndim, dims);
+}
+
+template <typename T>
+void get_attribute(json &obj, string name, size_t &disp, T &data)
+{
+  string dtype;
+  string desc;
+  size_t size;
+  get_metadata(obj, name, dtype, desc, disp, size);
+  data = obj[name]["data"].get<T>();
+}
+
+template <typename T>
+void get_attribute(json &obj, string name, size_t &disp, int32_t length, T *data)
+{
+  string dtype;
+  string desc;
+  size_t size;
+  get_metadata(obj, name, dtype, desc, disp, size);
+
+  std::vector<T> vec = obj[name]["data"].get<std::vector<T>>();
+  for (int i = 0; i < length; i++) {
+    data[i] = vec[i];
+  }
+}
+
+void put_attribute(json &obj, string name, const size_t disp, const int32_t data)
+{
+  put_metadata(obj, name, "i4", "", disp, sizeof(int32_t));
+  obj[name]["data"] = data;
+}
+
+void put_attribute(json &obj, string name, const size_t disp, const int64_t data)
+{
+  put_metadata(obj, name, "i8", "", disp, sizeof(size_t));
+  obj[name]["data"] = data;
+}
+
+void put_attribute(json &obj, string name, const size_t disp, const float32 data)
+{
+  put_metadata(obj, name, "f4", "", disp, sizeof(float32));
+  obj[name]["data"] = data;
+}
+
+void put_attribute(json &obj, string name, const size_t disp, const float64 data)
+{
+  put_metadata(obj, name, "f8", "", disp, sizeof(float64));
+  obj[name]["data"] = data;
+}
+
+void put_attribute(json &obj, string name, const size_t disp, const int32_t length,
+                   const int32_t *data)
+{
+  int32_t dims[1] = {length};
+  put_metadata(obj, name, "i4", "", disp, sizeof(int32_t) * length, 1, dims);
+  obj[name]["data"] = std::vector<int32_t>(&data[0], &data[length]);
+}
+
+void put_attribute(json &obj, string name, const size_t disp, const int32_t length,
+                   const int64_t *data)
+{
+  int32_t dims[1] = {length};
+  put_metadata(obj, name, "i8", "", disp, sizeof(size_t) * length, 1, dims);
+  obj[name]["data"] = std::vector<size_t>(&data[0], &data[length]);
+}
+
+void put_attribute(json &obj, string name, const size_t disp, const int32_t length,
+                   const float32 *data)
+{
+  int32_t dims[1] = {length};
+  put_metadata(obj, name, "f4", "", disp, sizeof(float32) * length, 1, dims);
+  obj[name]["data"] = std::vector<float32>(&data[0], &data[length]);
+}
+
+void put_attribute(json &obj, string name, const size_t disp, const int32_t length,
+                   const float64 *data)
+{
+  int32_t dims[1] = {length};
+  put_metadata(obj, name, "f8", "", disp, sizeof(float64) * length, 1, dims);
+  obj[name]["data"] = std::vector<float64>(&data[0], &data[length]);
+}
+
+template void read_subarray(MPI_File *fh, size_t *disp, void *data, const int32_t ndim,
+                            const int32_t gshape[], const int32_t lshape[], const int32_t offset[],
+                            const int32_t elembyte, const int order);
+template void read_subarray(MPI_File *fh, size_t *disp, void *data, const size_t ndim,
+                            const size_t gshape[], const size_t lshape[], const size_t offset[],
+                            const size_t elembyte, const int order);
+template void write_subarray(MPI_File *fh, size_t *disp, void *data, const int32_t ndim,
+                             const int32_t gshape[], const int32_t lshape[], const int32_t offset[],
+                             const int32_t elembyte, const int order);
+template void write_subarray(MPI_File *fh, size_t *disp, void *data, const size_t ndim,
+                             const size_t gshape[], const size_t lshape[], const size_t offset[],
+                             const size_t elembyte, const int order);
+template void get_attribute(json &obj, string name, size_t &disp, int32_t &data);
+template void get_attribute(json &obj, string name, size_t &disp, int64_t &data);
+template void get_attribute(json &obj, string name, size_t &disp, float32 &data);
+template void get_attribute(json &obj, string name, size_t &disp, float64 &data);
+template void get_attribute(json &obj, string name, size_t &disp, int32_t length, int32_t *data);
+template void get_attribute(json &obj, string name, size_t &disp, int32_t length, int64_t *data);
+template void get_attribute(json &obj, string name, size_t &disp, int32_t length, float32 *data);
+template void get_attribute(json &obj, string name, size_t &disp, int32_t length, float64 *data);
 
 } // namespace jsonio
 
--- jsonio.hpp
+++ jsonio.hpp
@@ -2,235 +2,344 @@
 #ifndef _JSONIO_HPP_
 #define _JSONIO_HPP_
 
-///
-/// I/O module with JSON and MPI-IO
-///
-/// $Id$
-///
-#include "common.hpp"
-#include "tinyformat.hpp"
 #include <mpi.h>
 #include <nlohmann/json.hpp>
 
+///
+/// @brief JSON and MPI-IO utility module
+///
 namespace jsonio
 {
+using json    = nlohmann::ordered_json;
 using float32 = float;
 using float64 = double;
-using std::cerr;
-using std::endl;
 using std::string;
-using tfm::format;
-using json = nlohmann::ordered_json;
 
+///
+/// @brief primitive collective contiguous read/write with MPI-IO
+/// @param fh
+/// @param disp
+/// @param data
+/// @param offset
+/// @param size
+/// @param elembyte
+/// @param packbyte
+/// @param mode
+///
 void readwrite_contiguous(MPI_File *fh, size_t *disp, void *data, const size_t offset,
                           const size_t size, const int32_t elembyte, const int32_t packbyte,
                           const int mode);
 
+///
+/// @brief primitive non-collective contiguous read/write with MPI-IO
+/// @param fh
+/// @param disp
+/// @param data
+/// @param size
+/// @param elembyte
+/// @param packbyte
+/// @param req
+/// @param mode
+///
 void readwrite_contiguous_at(MPI_File *fh, size_t *disp, void *data, const size_t size,
                              const int32_t elembyte, const int32_t packbyte, MPI_Request *req,
                              const int mode);
 
+///
+/// @brief primitive collective read/write of subarray with MPI-IO
+/// @param fh
+/// @param disp
+/// @param data
+/// @param ndim
+/// @param gshape
+/// @param lshape
+/// @param offset
+/// @param elembyte
+/// @param mode
+/// @param order
+///
 void readwrite_subarray(MPI_File *fh, size_t *disp, void *data, const int32_t ndim,
                         const int32_t gshape[], const int32_t lshape[], const int32_t offset[],
                         const int32_t elembyte, const int mode, const int order);
 
+///
+/// @brief collective open file with MPI-IO
+/// @param filename
+/// @param fh
+/// @param disp
+/// @param mode
+///
 void open_file(const char *filename, MPI_File *fh, size_t *disp, const char *mode);
 
+///
+/// @brief collective close file with MPI-IO
+/// @param fh
+///
 void close_file(MPI_File *fh);
 
+///
+/// @brief non-collective read
+/// @param fh
+/// @param disp
+/// @param data
+/// @param size
+///
 void read_single(MPI_File *fh, size_t *disp, void *data, const size_t size);
 
+///
+/// @brief non-collective write
+/// @param fh
+/// @param disp
+/// @param data
+/// @param size
+///
 void write_single(MPI_File *fh, size_t *disp, void *data, const size_t size);
 
+///
+/// @brief collective contiguous read
+/// @param fh
+/// @param disp
+/// @param data
+/// @param size
+/// @param elembyte
+/// @param packbyte
+///
 void read_contiguous(MPI_File *fh, size_t *disp, void *data, const size_t size,
                      const int32_t elembyte, const int32_t packbyte = -1);
 
+///
+/// @brief collective contiguous write
+/// @param fh
+/// @param disp
+/// @param data
+/// @param size
+/// @param elembyte
+/// @param packbyte
+///
 void write_contiguous(MPI_File *fh, size_t *disp, void *data, const size_t size,
                       const int32_t elembyte, const int32_t packbyte = -1);
 
+///
+/// @brief non-blocking contiguous read
+/// @param fh
+/// @param disp
+/// @param data
+/// @param size
+/// @param elembyte
+/// @param req
+///
 void read_contiguous_at(MPI_File *fh, size_t *disp, void *data, const size_t size,
                         const int32_t elembyte, MPI_Request *req);
 
+///
+/// @brief non-blocking contiguous write
+/// @param fh
+/// @param disp
+/// @param data
+/// @param size
+/// @param elembyte
+/// @param req
+///
 void write_contiguous_at(MPI_File *fh, size_t *disp, void *data, const size_t size,
                          const int32_t elembyte, MPI_Request *req);
 
+///
+/// @brief collective read of subarray
+/// @tparam T1
+/// @tparam T2
+/// @tparam T3
+/// @tparam T4
+/// @tparam T5
+/// @param fh
+/// @param disp
+/// @param data
+/// @param ndim
+/// @param gshape
+/// @param lshape
+/// @param offset
+/// @param elembyte
+/// @param order
+///
 template <typename T1, typename T2, typename T3, typename T4, typename T5>
 void read_subarray(MPI_File *fh, size_t *disp, void *data, const T1 ndim, const T2 gshape[],
                    const T3 lshape[], const T4 offset[], const T5 elembyte,
-                   const int order = MPI_ORDER_C)
-{
-  if (order != MPI_ORDER_C && order != MPI_ORDER_FORTRAN) {
-    cerr << format("Error: No such order available\n");
-  }
+                   const int order = MPI_ORDER_C);
 
-  // convert to int32_t for MPI call
-  int32_t nd = static_cast<int32_t>(ndim);
-  int32_t eb = static_cast<int32_t>(elembyte);
-  int32_t gs[nd], ls[nd], os[nd];
-
-  size_t size = 1;
-  for (int i = 0; i < nd; i++) {
-    gs[i] = static_cast<int32_t>(gshape[i]);
-    ls[i] = static_cast<int32_t>(lshape[i]);
-    os[i] = static_cast<int32_t>(offset[i]);
-    size *= gshape[i];
-  }
-
-  readwrite_subarray(fh, disp, data, nd, gs, ls, os, eb, +1, order);
-  *disp += size * elembyte;
-}
-
+///
+/// @brief collective write of subarray
+/// @tparam T1
+/// @tparam T2
+/// @tparam T3
+/// @tparam T4
+/// @tparam T5
+/// @param fh
+/// @param disp
+/// @param data
+/// @param ndim
+/// @param gshape
+/// @param lshape
+/// @param offset
+/// @param elembyte
+/// @param order
+///
 template <typename T1, typename T2, typename T3, typename T4, typename T5>
 void write_subarray(MPI_File *fh, size_t *disp, void *data, const T1 ndim, const T2 gshape[],
                     const T3 lshape[], const T4 offset[], const T5 elembyte,
-                    const int order = MPI_ORDER_C)
-{
-  if (order != MPI_ORDER_C && order != MPI_ORDER_FORTRAN) {
-    cerr << format("Error: No such order available\n");
-  }
+                    const int order = MPI_ORDER_C);
 
-  // convert to int32_t for MPI call
-  int32_t nd = static_cast<int32_t>(ndim);
-  int32_t eb = static_cast<int32_t>(elembyte);
-  int32_t gs[nd], ls[nd], os[nd];
+///
+/// @brief put metadata of array to json object
+/// @param obj
+/// @param name
+/// @param dtype
+/// @param desc
+/// @param disp
+/// @param size
+/// @param ndim
+/// @param dims
+///
+void put_metadata(json &obj, string name, string dtype, string desc, const size_t disp,
+                  const size_t size, const int32_t ndim, const int32_t dims[]);
 
-  size_t size = 1;
-  for (int i = 0; i < nd; i++) {
-    gs[i] = static_cast<int32_t>(gshape[i]);
-    ls[i] = static_cast<int32_t>(lshape[i]);
-    os[i] = static_cast<int32_t>(offset[i]);
-    size *= gshape[i];
-  }
+///
+/// @brief put metadata of scalar to json object
+/// @param obj
+/// @param name
+/// @param dtype
+/// @param desc
+/// @param disp
+/// @param size
+///
+void put_metadata(json &obj, string name, string dtype, string desc, const size_t disp,
+                  const size_t size);
 
-  readwrite_subarray(fh, disp, data, nd, gs, ls, os, eb, -1, order);
-  *disp += size * elembyte;
-}
+///
+/// @brief get metadata of array from json object
+/// @param obj
+/// @param name
+/// @param dtype
+/// @param desc
+/// @param disp
+/// @param size
+/// @param ndim
+/// @param dims
+///
+void get_metadata(json &obj, string name, string &dtype, string &desc, size_t &disp, size_t &size,
+                  int32_t &ndim, int32_t dims[]);
 
-inline void put_metadata(json &obj, string name, string dtype, string desc, const size_t disp,
-                         const size_t size, const int32_t ndim, const int32_t dims[])
-{
-  obj[name]["datatype"]    = dtype;
-  obj[name]["description"] = desc;
-  obj[name]["offset"]      = disp;
-  obj[name]["size"]        = size;
-  obj[name]["ndim"]        = ndim;
-  obj[name]["shape"]       = json::array();
+///
+/// @brief get metadata of scalar from json object
+/// @param obj
+/// @param name
+/// @param dtype
+/// @param desc
+/// @param disp
+/// @param size
+///
+void get_metadata(json &obj, string name, string &dtype, string &desc, size_t &disp, size_t &size);
 
-  for (int i = 0; i < ndim; i++) {
-    obj[name]["shape"].push_back(dims[i]);
-  }
-}
-
-inline void put_metadata(json &obj, string name, string dtype, string desc, const size_t disp,
-                         const size_t size)
-{
-  const int ndim    = 1;
-  const int dims[1] = {1};
-  put_metadata(obj, name, dtype, desc, disp, size, ndim, dims);
-}
-
-inline void get_metadata(json &obj, string name, string &dtype, string &desc, size_t &disp,
-                         size_t &size, int32_t &ndim, int32_t dims[])
-{
-  dtype = obj[name]["datatype"].get<string>();
-  desc  = obj[name]["description"].get<string>();
-  disp  = obj[name]["offset"].get<size_t>();
-  size  = obj[name]["size"].get<size_t>();
-  ndim  = obj[name]["ndim"].get<int>();
-
-  auto v = obj[name]["shape"];
-  for (int i = 0; i < ndim; i++) {
-    dims[i] = v[i].get<int>();
-  }
-}
-
-inline void get_metadata(json &obj, string name, string &dtype, string &desc, size_t &disp,
-                         size_t &size)
-{
-  int32_t ndim;
-  int32_t dims[1];
-  get_metadata(obj, name, dtype, desc, disp, size, ndim, dims);
-}
-
+///
+/// @brief get scalar attribute from json object
+/// @tparam T
+/// @param obj
+/// @param name
+/// @param disp
+/// @param data
+///
 template <typename T>
-inline void get_attribute(json &obj, string name, size_t &disp, T &data)
-{
-  string dtype;
-  string desc;
-  size_t size;
-  get_metadata(obj, name, dtype, desc, disp, size);
-  data = obj[name]["data"].get<T>();
-}
+void get_attribute(json &obj, string name, size_t &disp, T &data);
 
+///
+/// @brief get array attribute from json object
+/// @tparam T
+/// @param obj
+/// @param name
+/// @param disp
+/// @param length
+/// @param data
+///
 template <typename T>
-inline void get_attribute(json &obj, string name, size_t &disp, int32_t length, T *data)
-{
-  string dtype;
-  string desc;
-  size_t size;
-  get_metadata(obj, name, dtype, desc, disp, size);
+void get_attribute(json &obj, string name, size_t &disp, int32_t length, T *data);
 
-  std::vector<T> vec = obj[name]["data"].get<std::vector<T>>();
-  for (int i = 0; i < length; i++) {
-    data[i] = vec[i];
-  }
-}
+///
+/// @brief put 32bit integer scalar attribute
+/// @param obj
+/// @param name
+/// @param disp
+/// @param data
+///
+void put_attribute(json &obj, string name, const size_t disp, const int32_t data);
 
-inline void put_attribute(json &obj, string name, const size_t disp, const int32_t data)
-{
-  put_metadata(obj, name, "i4", "", disp, sizeof(int32_t));
-  obj[name]["data"] = data;
-}
+///
+/// @brief put 64bit integer scalar attribute
+/// @param obj
+/// @param name
+/// @param disp
+/// @param data
+///
+void put_attribute(json &obj, string name, const size_t disp, const int64_t data);
 
-inline void put_attribute(json &obj, string name, const size_t disp, const int64_t data)
-{
-  put_metadata(obj, name, "i8", "", disp, sizeof(size_t));
-  obj[name]["data"] = data;
-}
+///
+/// @brief put 32bit real scalar attribute
+/// @param obj
+/// @param name
+/// @param disp
+/// @param data
+///
+void put_attribute(json &obj, string name, const size_t disp, const float32 data);
 
-inline void put_attribute(json &obj, string name, const size_t disp, const float32 data)
-{
-  put_metadata(obj, name, "f4", "", disp, sizeof(float32));
-  obj[name]["data"] = data;
-}
+///
+/// @brief put 64bit real scalar attribute
+/// @param obj
+/// @param name
+/// @param disp
+/// @param data
+///
+void put_attribute(json &obj, string name, const size_t disp, const float64 data);
 
-inline void put_attribute(json &obj, string name, const size_t disp, const float64 data)
-{
-  put_metadata(obj, name, "f8", "", disp, sizeof(float64));
-  obj[name]["data"] = data;
-}
+///
+/// @brief put 32bit integer array attribute
+/// @param obj
+/// @param name
+/// @param disp
+/// @param length
+/// @param data
+///
+void put_attribute(json &obj, string name, const size_t disp, const int32_t length,
+                   const int32_t *data);
 
-inline void put_attribute(json &obj, string name, const size_t disp, const int32_t length,
-                          const int32_t *data)
-{
-  int32_t dims[1] = {length};
-  put_metadata(obj, name, "i4", "", disp, sizeof(int32_t) * length, 1, dims);
-  obj[name]["data"] = std::vector<int32_t>(&data[0], &data[length]);
-}
+///
+/// @brief put 64bit integer array attribute
+/// @param obj
+/// @param name
+/// @param disp
+/// @param length
+/// @param data
+///
+void put_attribute(json &obj, string name, const size_t disp, const int32_t length,
+                   const int64_t *data);
 
-inline void put_attribute(json &obj, string name, const size_t disp, const int32_t length,
-                          const int64_t *data)
-{
-  int32_t dims[1] = {length};
-  put_metadata(obj, name, "i8", "", disp, sizeof(size_t) * length, 1, dims);
-  obj[name]["data"] = std::vector<size_t>(&data[0], &data[length]);
-}
+///
+/// @brief put 32bit real array attribute
+/// @param obj
+/// @param name
+/// @param disp
+/// @param length
+/// @param data
+///
+void put_attribute(json &obj, string name, const size_t disp, const int32_t length,
+                   const float32 *data);
 
-inline void put_attribute(json &obj, string name, const size_t disp, const int32_t length,
-                          const float32 *data)
-{
-  int32_t dims[1] = {length};
-  put_metadata(obj, name, "f4", "", disp, sizeof(float32) * length, 1, dims);
-  obj[name]["data"] = std::vector<float32>(&data[0], &data[length]);
-}
-
-inline void put_attribute(json &obj, string name, const size_t disp, const int32_t length,
-                          const float64 *data)
-{
-  int32_t dims[1] = {length};
-  put_metadata(obj, name, "f8", "", disp, sizeof(float64) * length, 1, dims);
-  obj[name]["data"] = std::vector<float64>(&data[0], &data[length]);
-}
+///
+/// @brief put 64bit real array attribute
+/// @param obj
+/// @param name
+/// @param disp
+/// @param length
+/// @param data
+///
+void put_attribute(json &obj, string name, const size_t disp, const int32_t length,
+                   const float64 *data);
 
 } // namespace jsonio
 
